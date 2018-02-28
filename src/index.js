10210cd7b6024d0a009d91d26ac8c72c64f57eeb
Arseniya <goldstandart165@yandex.ru>
2018-02-28 15:51:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var inc = 0;
+  while (number > 5) {
+  number = Math.floor((number / 5)); inc = inc + number; 
+  }
+  return inc;
 }
+/*
+  function factorial(num)
+{
+    var rval=1;
+    for (var i = 2; i <= num; i++)
+    rval = rval * i;
+    return rval;
+}
+  var number = factorial(number);
+  var array = Array.from(number.toString()); 
+  var inc = 0;
+    for(var i=0; i<array.length;i++ )
+      { 
+        if(array[i] == 0)
+      {
+         inc ++; 
+      }
+} 
+return inc;
+*/
+
